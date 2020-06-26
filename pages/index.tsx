51b95d9a4813636d9087fee7d21f3cb0d501fc3a
frse97 <s.fries@wbi.at>
2020-06-26 20:10:41 +0200

    added Head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,32 @@
-const Home = () => {
-    return <div>FRSE's Page</div>
-}
+import Head from "next/head";
 
-export default Home;
+/**
+ * The <code>`App`</code> is used to render the landing page.
+ *
+ * The `<title></title>` tag defined document title
+ * - Provides title for page
+ * - Provides title when added to favourites
+ * - Provides title for page in search engine results
+ *
+ * The `<meta></meta>` tag defines metadata.
+ * - `viewport` Is used to define the viewport
+ * - `description` Is used to describe the site
+ * - `keywords` Is used to describe keywords for search engines (SEO)
+ *              what keywords are allowed? Rights?
+ */
+
+const App = () => {
+  return (
+    <>
+      <Head>
+        <title>frse</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="frse Portofolio" />
+        <meta name="keywords" content="frse, FRSE, portfolio, next.js" />
+      </Head>
+      <div>FRSE's Page</div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/FrseSection/FrseSection.tsx
@@ -0,0 +1,9 @@
+interface IFrseSectionProps {
+
+}
+
+const FrseSection: React.FC<IFrseSectionProps> = props => {
+  return <section></section>;
+};
+
+export default FrseSection;

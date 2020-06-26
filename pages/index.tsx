11710c58e1443d78eca4ad1bcef9a08b48872a0f
frse97 <sebastianfries@outlook.com>
2020-06-26 20:41:11 +0200

    Adapted meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,9 @@ const App = () => {
     <>
       <Head>
         <title>frse</title>
+        <meta name="UTF-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="Sebastian Fries (FRSE)" />
         <meta name="description" content="frse Portofolio" />
         <meta name="keywords" content="frse, FRSE, portfolio, next.js" />
       </Head>

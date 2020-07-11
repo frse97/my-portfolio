9aa33462e74a8e653396db141c2496f56fad6ee8
frse97 <sebastianfries@outlook.com>
2020-07-11 21:10:53 +0200

    Added Gloria Font.

--- src/components/FrsePortfolio/FrsePortfolio.tsx
+++ src/components/FrsePortfolio/FrsePortfolio.tsx
@@ -11,7 +11,6 @@ import './FrsePortfolio.less';
 
 // function FrsePortfolio({ Component, pageProps }) {
 const FrsePortfolio: React.FC<IFrsePortfolio> = (props) => {
-
   const classNames = cs('frse-portfolio');
 
   return (
@@ -20,6 +19,12 @@ const FrsePortfolio: React.FC<IFrsePortfolio> = (props) => {
       <Page>
         <Main>
           <Section>
+            <h1 style={{textAlign: 'center'}}>
+              <span>Hi! I'm Sebastian.</span>
+              <span> Junior frontend Developer,</span>
+              <span> drunk Phylosophist</span>
+              <span> and artist.</span>
+            </h1>
           </Section>
         </Main>
       </Page>
